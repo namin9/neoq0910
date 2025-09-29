bb10e237347b02f082727109bbfa9c9366814c19
namin9 <96509320+namin9@users.noreply.github.com>
2025-09-29 16:55:06 +0900

    Update _middleware.ts

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,7 +1,5 @@
 export const onRequest: PagesFunction = async ({ request, next }) => {
   const { pathname } = new URL(request.url);
-
-  if (pathname.startsWith("/api/")) return next();
-
+  if (pathname.startsWith("/api/")) return next(); // API는 공개
   return next();
 };
