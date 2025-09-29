0276e09210d262e7d051b9fce6f62fde83ebf0fc
namin9 <96509320+namin9@users.noreply.github.com>
2025-09-29 15:02:17 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,3 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: { outDir: 'dist' }
-})
+export default defineConfig({ plugins: [react()], base: '/', build: { outDir: 'dist' } })
