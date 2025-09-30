0776d835885e43585deddc9e65f07cb0092c008f
namin9 <96509320+namin9@users.noreply.github.com>
2025-09-30 18:17:58 +0900

    Fix Vite config object syntax

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: { outDir: 'dist' }
   publicDir: 'public',
   build: {
     outDir: 'dist',
-    copyPublicDir: true
-  }
+    copyPublicDir: true,
+  },
 })
